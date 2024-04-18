77ca4d65185bc8cb5fd556a43c4efa63b2f79573
ZjuulOWOW <zjuul@owow.io>
2024-04-18 16:46:15 +0200

    user test setup

---
+++ main.js
@@ -0,0 +1,9 @@
+// TO DO:
+// 1. Install GSAP Motion and necessary dependencies.
+// 2. Add the Marquee animation to an element.
+// 3. Add the Parallax animation to an element.
+// 4. Add the Diorama animation to an element.
+// 5. Speed up the Marquee animation. Then, ensure that the direction and speed of the animation are based on scroll behavior.
+// 6. Log the progress of the Marquee animation.
+// 7. Make the markers (GSAP) visible for the Parallax animation. Then, ensure that the animation starts later.
+// 8. Log the normalized positions (nx, ny) of the mouse position in the client (window) using the mousePosition utility.
